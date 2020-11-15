5f412ebabf636c75a3ca4c614b6b3d2510430b27
groot <farhanch101@outlook.com>
2020-11-16 03:35:17 +0500

    App file with styling

--- src/app.js
+++ src/app.js
@@ -1,2 +1,15 @@
-console.log("This is a react app");
+import React from "react";
+import ReactDOM from "react-dom";
+import "./app.css";
 
+const App = () => (
+  <div className="App">
+    <header>
+      <h1>Hello react</h1>
+    </header>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+export default App;
